b0ba0d147e9a3067d677700e7da9bcaa5719c506
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-06-26 23:04:19 -0400

    Integrate matrix opening before React

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import shirousagi from './shirousagi';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+(async () => {
+  // Intro
+  await shirousagi();
+
+  // Remove canvas
+  const canvas = document.getElementById('introCanvas');
+  document.body.removeChild(canvas);
+
+  // Start React
+  const root = document.getElementById('root');
+  root.style.display = 'block';
+  ReactDOM.render(<App />, root);
+  registerServiceWorker();
+})();
