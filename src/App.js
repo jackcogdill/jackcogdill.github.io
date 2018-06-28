f1100f082bd3a24fcb15fd1562df5a773ce81212
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-06-27 23:29:15 -0400

    Add some basic content

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div className="App-name">Jack Cogdill</div>
-          <div className="App-location">Virginia Tech</div>
+        <header className="header">
+          <h1 className="header-title">My name is Jack Cogdill.</h1>
+          <h2 className="header-description">I'm a student at <a href="https://vt.edu/" target="_blank" rel="noopener noreferrer">Virginia Tech</a> studying Computer Science.</h2>
         </header>
-        <p className="App-intro">
-          Welcome to my website. I hope you enjoy your stay.
-        </p>
+        <div className="content">
+          <p className="intro">
+            Welcome to my website. I hope you enjoy your stay.
+          </p>
+        </div>
+        <footer className="footer">
+          <span>Jack Cogdill</span>
+          <div className="footer-links">
+            <a href="mailto:jackcog@vt.edu" target="_blank" rel="noopener noreferrer">Email</a>
+            <a href="https://github.com/jackcogdill" target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="files/cv.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+            <a href="https://www.instagram.com/jackcogdill/" target="_blank" rel="noopener noreferrer">Instagram</a>
+          </div>
+        </footer>
       </div>
     );
   }
