16246f38527160c5b941cf537ead2e90def80a1f
Jack Cogdill <jackcogdill@users.noreply.github.com>
2019-03-28 16:37:10 -0400

    Render allergies in markdown

--- src/Allergies.jsx
+++ src/Allergies.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 import './Allergies.css';
+import Markdown from './other/allergies.md';
 
-const Allergies = () => (
-  <div className="allergies">
-    test allergies
-  </div>
-);
+class Allergies extends Component {
+  constructor() {
+    super();
+    this.state = {
+      markdown: null,
+    };
+  }
+
+  componentWillMount() {
+    fetch(Markdown)
+      .then(res => res.text())
+      .then(raw => this.setState({
+        markdown: raw,
+      }));
+  }
+
+  render() {
+    const { markdown } = this.state;
+    return (
+      <div className="allergies">
+        <ReactMarkdown
+          source={markdown}
+          escapeHtml={false}
+          className="markdown"
+        />
+      </div>
+    );
+  }
+}
 
 export default Allergies;
