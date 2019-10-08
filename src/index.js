3927ca71e032f35299a52a29e2e3482444b6ca53
Jack Cogdill <jackcogdill@users.noreply.github.com>
2019-10-07 21:50:53 -0700

    Don't preload images

--- src/index.js
+++ src/index.js
@@ -5,26 +5,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Shirousagi from './shirousagi';
 
-const imageReferences = {};
-
-const preloadImages = () => {
-  const importAll = r => r.keys().map(r);
-  const images = importAll(require.context('./images', false, /^\.\//));
-
-  images.forEach((file) => {
-    const image = new Image();
-    image.src = file;
-    imageReferences[file] = image;
-  });
-};
-
 (async () => {
   const isHome = (
     window.location.hash === '' ||
     window.location.hash === '#/');
 
   if (isHome) {
-    preloadImages();
     await Shirousagi.intro();
   }
 
