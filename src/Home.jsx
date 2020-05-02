77fb32f519c7b8ec6cbca6110028dca036352b3f
Jack Cogdill <jackcogdill@users.noreply.github.com>
2020-05-02 11:31:35 -0700

    More readable text code

--- src/Home.jsx
+++ src/Home.jsx
@@ -28,68 +28,40 @@ const Photo = () => (
   </div>
 );
 
-const About = () => (
-  <div className="about">
-    <p>
-      Hi there!
-    </p>
-    <p>
-      I am a recent graduate of
-      {' '}
-      <A href="https://vt.edu/">
-        Virginia Tech
-      </A>
-      {' '}
-      where I studied Computer Science and Japanese.
-      Currently I live in the
-      {' '}
-      <A href="https://www.google.com/maps/place/San+Francisco+Bay+Area,+CA">
-        South Bay
-      </A>
-      {' '}
-      and am a front-end software engineer at
-      {' '}
-      <A href="https://about.google/">
-        Google
-      </A>
-      .
-    </p>
-    <p>
-      I have a lot of
-      {' '}
-      <Link to="/allergies">
-        food allergies
-      </Link>
-      .
-    </p>
-    <p>
-      Feel free to
-      {' '}
-      <A href="mailto:hi@jackcogdill.com">
-        email
-      </A>
-      {' '}
-      me; happy to chat about books, anime, philosophy, or anything else really.
-    </p>
-    <p>
-      Some more links:
-    </p>
-    <div className="contact-links">
-      <A href={Resume}>
-        résumé
-      </A>
-      <A href="https://github.com/jackcogdill">
-        GitHub
-      </A>
-      <A href="https://codepen.io/jackcogdill">
-        CodePen
-      </A>
-      <A href="https://www.linkedin.com/in/jackcogdill/">
-        LinkedIn
-      </A>
+const About = () => {
+  const school = <A href="https://vt.edu/">Virginia Tech</A>;
+  const location = <A href="https://www.google.com/maps/place/San+Francisco+Bay+Area,+CA">South Bay</A>;
+  const job = <A href="https://about.google/">Google</A>;
+  const allergies = <Link to="/allergies">food allergies</Link>;
+  const email = <A href="mailto:hi@jackcogdill.com">email</A>;
+  return (
+    <div className="about">
+      <p>
+        Hi there!
+      </p>
+      <p>
+        I am a recent graduate of {school} where I studied Computer Science and Japanese.
+        Currently I live in the {location} and am a front-end software engineer at {job}.
+      </p>
+      <p>
+        I have a lot of {allergies}.
+      </p>
+      <p>
+        Feel free to {email} me; happy to chat about books, anime, philosophy,
+        or anything else really.
+      </p>
+      <p>
+        Some more links:
+      </p>
+      <div className="contact-links">
+        <A href={Resume}>résumé</A>
+        <A href="https://github.com/jackcogdill">GitHub</A>
+        <A href="https://codepen.io/jackcogdill">CodePen</A>
+        <A href="https://www.linkedin.com/in/jackcogdill/">LinkedIn</A>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const Home = () => (
   <div className="home">
